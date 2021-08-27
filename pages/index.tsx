814bb942501d057127298d59ba0d0a7140339ca7
Yerkanat <yerkanat.makhayev@gmail.com>
2021-08-27 13:59:31 +0600

    as

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,13 +51,14 @@ export default function Home({res}: InferGetStaticPropsType<typeof getStaticProp
       <div className = {styles.author}>
         Tim-Berners Lee
       </div>
+      
       <hr className = {styles.line}>
       </hr>
       <h1 className = {styles.upcomingEvents}>Upcoming events</h1>
       <div className = {styles.flexContainer}> 
         <div className = {styles.featuredImage}>
       <Image layout = 'intrinsic' src = {res.imglink} width = {1000} height = {1000}  /> 
-      
+
       </div>
       <div className = {styles.featuredText}>
           <div className = {styles.postname}>
