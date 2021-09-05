198e67b8144069c4f9d5c25ffd6c1d15d81be0c8
Yerkanat <yerkanat.makhayev@gmail.com>
2021-09-05 23:25:38 +0600

    page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout>
+import {motion} from 'framer-motion'
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return  (
+        <motion.div key = {router.route} initial = "pageInitial" animate = "pageAnime" variants = {{
+          pageInitial: {
+            translateX: "100%",
+            transitionDuration: '0.1s'
+            
+          },
+          pageAnime: {
+            translateX: 0,
+            transitionDuration: '0.1s'
+            
+          },
+        }
+          
+        }>
+        <Layout>
             <Component {...pageProps} />
         </Layout>
-  
+        </motion.div>
+
+      
+        )
+
 }
 export default MyApp
